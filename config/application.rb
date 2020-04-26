ef590f018798c08bae9ff193a3feca3e0b0accb6
= <=>
2020-04-26 21:34:15 +0200

    Setup vanillar rails with rspec testing

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TestDrivenRails
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    
+
+   config.generators do |g|
+     g.test_framework :rspec,
+       fixtures: true,
+       view_specs: false,
+       helper_specs: true,
+       routing_specs: false,
+       controller_specs: true,
+       request_specs: false
+     g.fixture_replacement :factory_bot, dir: "spec/factories"
+   end
+
+   config.factory_bot.definition_file_paths = ["spec/factories"]    
+  end
+end
